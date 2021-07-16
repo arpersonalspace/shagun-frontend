e9b0cfa9903ba3f1b8485242724e54765feb8217
lokeshgaria <lokeshgaria8811@gmail.com>
2021-07-16 23:04:29 +0530

    created netflix clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDFTMY82PmiTT5LWq1EmQA_44yCsLfnUtc",
+    authDomain: "netflix-clone-6de73.firebaseapp.com",
+    projectId: "netflix-clone-6de73",
+    storageBucket: "netflix-clone-6de73.appspot.com",
+    messagingSenderId: "940859991564",
+    appId: "1:940859991564:web:52f0091252d03d77293029",
+    measurementId: "G-BMJ016HK0Q"
+  };
